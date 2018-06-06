54df0aebce0eb7df36a8692f1263bb79bedc1110
RAYSSA GABRIELI DOS SANTOS <rayssa.santos@univille.br>
2018-06-06 19:56:59 -0300

    aula

---
+++ src/main/java/com/univille/projdacs2018spring/ProjDacs2018SpringApplication.java
@@ -0,0 +1,12 @@
+package com.univille.projdacs2018spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDacs2018SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDacs2018SpringApplication.class, args);
+	}
+}
